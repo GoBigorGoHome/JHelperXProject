175cd8ee85f6390f31c46843e83b016262ebae22
zjsdut <zjsdut@163.com>
2020-06-20 12:49:17 +0800

    Add inline to functions

--- library/bit.hpp
+++ library/bit.hpp
@@ -1,17 +1,22 @@
 #include <cassert>
-template <typename int_t> inline int_t lowbit(int_t x) { return x & -x; }
+template<typename int_t> inline int_t lowbit(int_t x) {
+  return x & -x;
+}
 
 inline int h_bit(unsigned long long x) {
   return int(sizeof(unsigned long long) * 8 - __builtin_clzll(x));
 }
 
-unsigned long long pow2(unsigned long long x) { // round up to power of 2
-  if (x == 0) return 1;
+unsigned long long pow2(unsigned long long x) {// round up to power of 2
+  if (x == 0)
+    return 1;
   return x == lowbit(x) ? x : 1ull << h_bit(x);
 }
 
-template <typename T> int bit(T a, int i) { return a >> i & 1; }
-template <typename T> T get_mid(T l, T r) {
+template<typename T> inline int bit(T a, int i) {
+  return a >> i & 1;
+}
+template<typename T> inline T get_mid(T l, T r) {
   assert(l <= r);
   return l + (r - l >> 1);
 }
