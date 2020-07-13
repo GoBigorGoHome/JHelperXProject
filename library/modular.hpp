17d034c4cc4ebd1fd8296aa22a50f0e9f0c3c898
zjsdut <zjsdut@163.com>
2020-07-13 16:57:32 +0800

    Remove global using-declaration

--- library/modular.hpp
+++ library/modular.hpp
@@ -2,16 +2,15 @@
 #define ALGO_MODULAR
 #include <iostream>
 #include <cassert>
-using std::swap;
 // tourist's modular-arithmetic class
 template<typename T> T inverse(T a, T m) {
   T u = 0, v = 1;
   while (a != 0) {
     T t = m / a;
     m -= t * a;
-    swap(a, m);
+    std::swap(a, m);
     u -= t * v;
-    swap(u, v);
+    std::swap(u, v);
   }
   assert(m == 1);
   return u;
