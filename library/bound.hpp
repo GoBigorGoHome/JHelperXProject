8b6921de50446c0f93a7870ef8ec5721ee50e620
zjsdut <zjsdut@163.com>
2020-07-21 01:51:15 +0800

    Add library bound
    Wrappers of std::lower_bound/std::upper_bound that return index of
    lower/upper bound of a given value in a given random-access container,
    e.g., std::vector, std::array, built-in array.
    
    The old function-like macros are removed.

---
+++ library/bound.hpp
@@ -0,0 +1,32 @@
+//
+// Created by zjsdu on 7/21/2020.
+//
+
+#ifndef JHELPER_EXAMPLE_PROJECT_LIBRARY_BOUND_HPP_
+#define JHELPER_EXAMPLE_PROJECT_LIBRARY_BOUND_HPP_
+#include <algorithm>
+// upper_bound: *ptr <= val | val < *ptr
+// lower_bound: *ptr < val | val <= *ptr
+// [ lower_bound(beg, end, val), upper_bound(beg, end, val) ) is
+// the sub-range of elements that "equal" to val.
+template<typename Sequence, typename Value>
+int lb(const Sequence &seq, const Value &val) {
+  return std::lower_bound(std::begin(seq), std::end(seq), val)
+      - std::begin(seq);
+}
+template<typename Sequence, typename Value>
+int ub(const Sequence &seq, const Value &val) {
+  return std::upper_bound(std::begin(seq), std::end(seq), val)
+      - std::begin(seq);
+}
+template<typename Sequence, typename Value, typename Compare>
+int lb(const Sequence &seq, const Value &val, Compare comp) {
+  return std::lower_bound(std::begin(seq), std::end(seq), val, comp)
+      - std::begin(seq);
+}
+template<typename Sequence, typename Value, typename Compare>
+int ub(const Sequence &seq, const Value &val, Compare comp) {
+  return std::upper_bound(std::begin(seq), std::end(seq), val, comp)
+      - std::begin(seq);
+}
+#endif// JHELPER_EXAMPLE_PROJECT_LIBRARY_BOUND_HPP_
