cd44a6c7c31243d0586f970309d0b38b70489583
zjsdut <zjsdut@163.com>
2020-11-08 18:44:15 +0800

    Add extended Euclid's algorithm
    Also add another version of 'factorize()' which returns a list of
    factors that are maximal powers of prime factors. Rename the old version
    which returns a list of (prime_factor, exponent) pairs as
    'factorize_p()'.

--- library/nt.hpp
+++ library/nt.hpp
@@ -17,7 +17,7 @@ std::vector<long long> divisors(long long n) {
   return res;
 }
 
-std::vector<std::pair<long long, int>> factorize(long long n) {
+std::vector<std::pair<long long, int>> factorize_p(long long n) {
   std::vector<std::pair<long long, int>> res;
   for (long long i = 2; i * i <= n; ++i) {
     int cnt = 0;
@@ -32,6 +32,22 @@ std::vector<std::pair<long long, int>> factorize(long long n) {
   if (n > 1) {
     res.emplace_back(n, 1);
   }
+  return res;
+}
+
+auto factorize(long long n) {
+  std::vector<long long> res;
+  for (long long i = 2; i * i <= n; ++i) {
+    long long t = 1;
+    while (n % i == 0) {
+      t *= i;
+      n /= i;
+    }
+    if (t > 1)
+      res.push_back(t);
+  }
+  if (n > 1)
+    res.push_back(n);
   return res;
 }
 
@@ -67,4 +83,18 @@ std::vector<int> get_mpf(int n) {
     }
   }
   return minp;
+}
+
+long long gcd(long long a, long long b, long long &x, long long &y) {
+  if (b == 0) {
+    x = 1;
+    y = 0;
+    return a;
+  }
+  long long t = gcd(b, a % b, y, x);
+  // y * b + x * (a % b) == gcd(a, b)
+  // y * b + x * (a - (a / b) * b) == gcd(a, b)
+  // (y - x * (a / b)) * b + x * a == gcd(a, b)
+  y -= x * (a / b);
+  return t;
 }
