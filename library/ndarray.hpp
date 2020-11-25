713aeee6b7de25ccf82cdf9a8862d28e9d0a709c
zjsdut <zjsdut@163.com>
2020-11-25 14:54:40 +0800

    Replace inherited constructors
    The problem of having only inherited constructors:
    gcc 10.2:
    An inherited constructor is not a candidate for initialization from an
    expression of the same or derived type
    clang 11:
    inherited constructor cannot be used to copy object
    See https://godbolt.org/z/Edfchd for more detail.
    
    See https://stackoverflow.com/q/28450803/6793559 for discussion.
    
    See https://timsong-cpp.github.io/cppwp/n4861/over.match.funcs#8 for
    standard specification.
    
    I cannot find rule on this topic in C++17 standard draft.
    
    I need learn more about constructors and initialization.

--- library/ndarray.hpp
+++ library/ndarray.hpp
@@ -10,14 +10,16 @@
 // Example: vec<2, int> a(2, 3, -1); vec<3, int> b(2, 3, 4);
 template<unsigned dimension, typename T>
 class vec : public std::vector<vec<dimension - 1, T>> {
-  using std::vector<vec<dimension - 1, T>>::vector;
-
  public:
+  vec() = default;
   template<typename... Args>
   vec(unsigned d, Args... args)
       : std::vector<vec<dimension - 1, T>>(d, vec<dimension - 1, T>(args...)) {}
 };
 template<typename T> class vec<1, T> : public std::vector<T> {
-  using std::vector<T>::vector;
+ public:
+  vec<1, T>() = default;
+  template<typename... Args>
+  vec<1, T>(Args &&... args) : std::vector<T>(std::forward<Args...>(args...)) {}
 };
 #endif// JHELPER_EXAMPLE_PROJECT_LIBRARY_NDARRAY_HPP_
