9c97c2c4efa7c41aa39993993954014f1fd6f12d
zjsdut <zjsdut@163.com>
2020-11-25 16:52:29 +0800

    Reformat implementation of segment tree
    1. Move init_leaf, modify_leaf and collect from class member to function
    parameter.
    2. Replace variadic parameter with Tag for modifying segment.
    3. Unify modifying leaf and modifying segment.
    4. Allow empty segment for get(), find_first() and find_last().

--- library/gxx_segtree.hpp
+++ library/gxx_segtree.hpp
@@ -16,42 +16,37 @@ template<typename Value, typename Tag, typename Result> class SegTree {
     int p = -1;
     Value val;
     Tag tag;
-    template<typename... M> void apply(int l, int r, const M &... v) {
-      if constexpr (Tag::additive) {
-        Tag delta{v...};
-        tag += delta;
-        val.apply(delta, l, r);
-      } else {
-        tag.apply(v...);
-      }
+    void apply(int l, int r, const Tag &t) {
+      tag.apply(t);
+      val.apply(l, r, t);
     }
   };
-  const Tag empty_tag{};
   int n;
   std::vector<Node> tree;
   inline void push(int l, int r) {
-    int x = index(l, r), m = (l + r) >> 1, y = index(l, m), z = index(m + 1, r);
-    if (tree[x].tag) {
-      tree[y].val.apply(tree[x].tag, l, m);
-      tree[y].tag += tree[x].tag;
-      tree[z].val.apply(tree[x].tag, m + 1, r);
-      tree[z].tag += tree[x].tag;
-      tree[x].tag = empty_tag;
+    auto &t = tree[index(l, r)].tag;
+    if (t.pushable()) {
+      int m = (l + r) >> 1, y = index(l, m), z = index(m + 1, r);
+      tree[y].val.apply(l, m, t);
+      tree[y].tag.apply(t);
+      tree[z].val.apply(m + 1, r, t);
+      tree[z].tag.apply(t);
+      t.after_push();
     }
   }
   inline void pull(int l, int r) {
     int x = index(l, r), m = (l + r) >> 1, y = index(l, m), z = index(m + 1, r);
     tree[x].val = tree[y].val + tree[z].val;
   }
-  void build(int l, int r) {
+  template<typename F> void build(int l, int r, const F &f) {
     if (l == r) {
-      tree[2 * l].val.init_leaf(l);
+      f(tree[2 * l].val, l);
       return;
     }
     int m = (l + r) >> 1;
     tree[index(l, m)].p = tree[index(m + 1, r)].p = l + r;
-    build(l, m);
-    build(m + 1, r);
+    build(l, m, f);
+    build(m + 1, r, f);
     pull(l, r);
   }
   Result get(int l, int r, int ll, int rr) {
@@ -65,18 +60,17 @@ template<typename Value, typename Tag, typename Result> class SegTree {
       return get(m + 1, r, ll, rr);
     return get(l, m, ll, rr) + get(m + 1, r, ll, rr);
   }
-  template<typename... M>
-  void modify(int l, int r, int ll, int rr, const M &... v) {
+  void modify(int l, int r, int ll, int rr, const Tag &t) {
     if (ll <= l && r <= rr) {
-      tree[index(l, r)].apply(l, r, v...);
+      tree[index(l, r)].apply(l, r, t);
       return;
     }
     push(l, r);
     int m = (l + r) >> 1;
     if (ll <= m)
-      modify(l, m, ll, rr, v...);
+      modify(l, m, ll, rr, t);
     if (rr > m)
-      modify(m + 1, r, ll, rr, v...);
+      modify(m + 1, r, ll, rr, t);
     pull(l, r);
   }
   int find_first(int l, int r, int ll, int rr,
@@ -125,29 +119,34 @@ template<typename Value, typename Tag, typename Result> class SegTree {
   }
 
  public:
-  SegTree(int _n) : n(_n) {
+  template<typename F = void (*)(Value &, int)>
+  SegTree(
+      int _n, const F &f = [](Value &, int) {})
+      : n(_n) {
     assert(n > 0);
     tree.resize(2 * n - 1);
-    build(0, n - 1);
+    build(0, n - 1, f);
   }
   Result get() { return tree[0].val; }
   Result get(int ll, int rr) {
-    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    if (ll > rr)
+      return Result{};
+    assert(0 <= ll && rr <= n - 1);
     return get(0, n - 1, ll, rr);
   }
   Result get(int p) {
     assert(0 <= p && p <= n - 1);
     return get(0, n - 1, p, p);
   }
-  template<typename... M> void modify(int ll, int rr, const M &... v) {
+  void modify(int ll, int rr, const Tag &t) {
     if (ll > rr)
       return;
     assert(0 <= ll && rr <= n - 1);
-    modify(0, n - 1, ll, rr, v...);
+    modify(0, n - 1, ll, rr, t);
   }
-  template<typename... M> void modify_leaf(int leaf, const M &... v) {
+  template<typename F> void modify(int leaf, const F &f) {
     assert(0 <= leaf && leaf < n);
-    tree[2 * leaf].val.apply_to_leaf(v...);
+    f(tree[2 * leaf].val);
     for (int l = leaf, r = leaf, s; (s = tree[index(l, r)].p) != -1;)
       if (s < l + r) {
         tree[s | 1] = tree[index(s - r, l - 1)] + tree[index(l, r)];
@@ -157,19 +156,23 @@ template<typename Value, typename Tag, typename Result> class SegTree {
         r = s - l;
       }
   }
-  template<typename... Args> void collect(int leaf, Args &&... args) const {
+  template<typename F> void collect(int leaf, const F &f) const {
     assert(0 <= leaf && leaf < n);
     for (int i = 2 * leaf; i != -1; i = tree[i].p | 1)
-      tree[i].tag.collect(std::forward<Args...>(args...));
+      f(tree[i].tag);
   }
   // find_first and find_last call all FALSE elements
   // to the left (right) of the sought position exactly once
   int find_first(int ll, int rr, const std::function<bool(const Value &)> &f) {
-    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    if (ll > rr)
+      return -1;
+    assert(0 <= ll && rr <= n - 1);
     return find_first(0, n - 1, ll, rr, f);
   }
   int find_last(int ll, int rr, const std::function<bool(const Value &)> &f) {
-    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    if (ll > rr)
+      return -1;
+    assert(0 <= ll && rr <= n - 1);
     return find_last(0, n - 1, ll, rr, f);
   }
 };
