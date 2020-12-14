392dee8d610de985b9943f4f7743def2ebc2f48d
zjsdut <zjsdut@163.com>
2020-12-14 14:01:52 +0800

    Modify constructor of vec
    Only the last dimension's extension can be omitted when initializing a
    vec object.

--- library/ndarray.hpp
+++ library/ndarray.hpp
@@ -6,12 +6,13 @@
 #include <vector>
 // arbitrary-dimensional array that allows non-constexpr extents.
 // Usage: v<dimension, value_type> arr(extents..., init_value);
-// You can specify some first extents or all extents and an initial value.
-// Example: vec<2, int> a(2, 3, -1); vec<3, int> b(2, 3, 4);
+// All extensions but one for the last dimension are required. An initial value
+// for all array items can be specified if all extensions are present.
+// Examples:
+// vec<2, int> a(2, 3, -1); vec<3, int> b(2, 3, 4); vec<3, int> c(2, 3);
 template<unsigned dimension, typename T>
 class vec : public std::vector<vec<dimension - 1, T>> {
  public:
-  vec() = default;
   template<typename... Args>
   vec(unsigned d, Args... args)
       : std::vector<vec<dimension - 1, T>>(d, vec<dimension - 1, T>(args...)) {}
