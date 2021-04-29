5a748cbd480ef31ee97f6d76bd0982f6fc0aba1b
Jiashu Zou <zjsdut@163.com>
2021-04-29 21:42:47 +0800

    Reimplement the grid facility class
    Features a much faster `neighbors` method to be used inside range-based for loops.

--- library/grid.hpp
+++ library/grid.hpp
@@ -4,31 +4,55 @@
 
 #ifndef JHELPER_EXAMPLE_PROJECT_LIBRARY_GRID_HPP_
 #define JHELPER_EXAMPLE_PROJECT_LIBRARY_GRID_HPP_
-#include <vector>
+#include <utility>
 #include <cassert>
 class grid {
-  int n, m;
+  const int n, m;
+  static constexpr int dx[] = {0, 0, -1, 1}, dy[] = {-1, 1, 0, 0};
 
  public:
-  grid(int row, int col) {
-    assert(row > 0 && col > 0);
-    n = row;
-    m = col;
-  }
-  inline bool cover(int i, int j) const {
-    return i >= 0 and i < n and j >= 0 and j < m;
-  }
-  static constexpr int dx[] = {0, 0, -1, 1}, dy[] = {-1, 1, 0, 0};
-  template<typename Predicate = bool (*)(int, int)>
-  inline std::vector<std::pair<int, int>> neighbors(
-      int x, int y, Predicate p = [](int x, int y) { return true; }) const {
-    assert(cover(x, y));
-    std::vector<std::pair<int, int>> res;
-    for (int i = 0; i < 4; i++) {
-      if (cover(x + dx[i], y + dy[i]) and p(x + dx[i], y + dy[i]))
-        res.emplace_back(x + dx[i], y + dy[i]);
+  grid(int height, int width) : n(height), m(width) { assert(n > 0 && m > 0); }
+  struct cell_neighbors;
+  struct neighbor_iterator {
+    int value;
+    const cell_neighbors *const center;
+    neighbor_iterator(int value, const cell_neighbors *center)
+        : value(value), center(center) {}
+    bool operator!=(const neighbor_iterator &other) const {
+      return value != other.value;
     }
-    return res;
+    std::pair<int, int> operator*() const {
+      assert(value != 4);
+      return {center->r + dx[value], center->c + dy[value]};
+    }
+    neighbor_iterator &operator++() {
+      while (value < 4) {
+        ++value;
+        int r = center->r + dx[value];
+        int c = center->c + dy[value];
+        if (center->g->cover(r, c))
+          break;
+      }
+      return *this;
+    }
+  };
+  struct cell_neighbors {
+    const grid *const g;
+    const int r, c;
+    neighbor_iterator begin() const {
+      neighbor_iterator it(-1, this);
+      return ++it;
+    }
+    neighbor_iterator end() const { return neighbor_iterator(4, this); }
+  };
+
+  inline bool cover(int i, int j) const {
+    return 0 <= i and i < n and 0 <= j and j < m;
+  }
+
+  inline cell_neighbors neighbors(int r, int c) const {
+    assert(cover(r, c));
+    return cell_neighbors{this, r, c};
   }
 };
 #endif// JHELPER_EXAMPLE_PROJECT_LIBRARY_GRID_HPP_
