6b9af90e0e72ac21b4def857b476c5bcd43a1c02
Jiashu Zou <zjsdut@163.com>
2021-10-13 13:35:53 +0800

    Rename macro mp to MP
    mp is sometimes used to name a map.

--- library/alias.hpp
+++ library/alias.hpp
@@ -70,7 +70,7 @@ template<typename T> struct range_tuple {
 // hat off to 300iq
 #define RALL(x) std::rbegin(x), std::rend(x)
 #define pb emplace_back
-#define mp make_pair
+#define MP make_pair
 #define ep emplace
 #define SZ(x) (int) (x).size()
 #define rp(...) return pl(__VA_ARGS__)
