0b3c707726805861a1b695b9ef41bb7fd46812d8
Jiashu Zou <zjsdut@163.com>
2021-10-21 16:39:12 +0800

    Add min_xor() and max_xor() to bitwise_trie
    Edit document of some methods.

--- library/bitwise_trie.hpp
+++ library/bitwise_trie.hpp
@@ -21,7 +21,7 @@ template<typename T> class bitwise_trie {
     assert(0 <= pos and pos < (int) data_.size());
     return &data_[pos];
   }
-  /// \anchor (next)
+
   /// @brief Returns the next position going from \p pos by character \p bit. If
   /// the next position does not exist, create a new node.
   /// @param bit is 0 or 1.
@@ -35,10 +35,58 @@ template<typename T> class bitwise_trie {
     return next_[pos][bit];
   }
 
-  ///@brief Similar to \c next. \n Will not create a new node if the required one
-  /// does not exist, returns -1 instead.
+  /// @brief Similar to \c next. \n Will not create a new node if the required
+  /// one does not exist, returns -1 instead.
   int next_pos(int current_pos, int bit) const {
     return next_[current_pos][bit];
+  }
+
+  /// @brief Insert a bit sequence of length \p h + 1 represented as an integer
+  /// \p key, starting at \p h-th bit of \p key.
+  template<typename Int> int insert(Int key, int h) {
+    int pos = 0;
+    for (int i = h; i >= 0; i--) {
+      pos = next(pos, (int) (key >> i & 1));
+    }
+    return pos;
+  }
+
+  /// @brief Find the minimum value of \p x XOR \em key over all keys in the
+  /// trie. All keys in the trie should have the same length \p h + 1.
+  template<typename Int> Int min_xor(Int x, int h) const {
+    Int result = 0;
+    int pos = 0;
+    for (int i = h; i >= 0; i--) {
+      int bit = (int) (x >> i & 1);
+      if (int t = next_pos(pos, bit); t != -1) {
+        pos = t;
+      } else {
+        pos = next_pos(pos, 1 - bit);
+        if (pos == -1)
+          return -1;
+        result |= (Int) 1 << i;
+      }
+    }
+    return result;
+  }
+
+  /// @brief Find the maximum value of \p x XOR \em key over all keys in the
+  /// trie. All keys in the trie should have the same length \p h + 1.
+  template<typename Int> Int max_xor(Int x, int h) const {
+    Int result = 0;
+    int pos = 0;
+    for (int i = h; i >= 0; i--) {
+      int bit = (int) (x >> i & 1);
+      if (int t = next(pos, 1 - bit); t != -1) {
+        pos = t;
+        result |= (Int) 1 << i;
+      } else {
+        pos = next_pos(pos, bit);
+        if (pos == -1)
+          return -1;
+      }
+    }
+    return result;
   }
 };
 #endif// JHELPER_EXAMPLE_PROJECT_LIBRARY_BITWISE_TRIE_HPP_
