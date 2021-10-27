dddfdfec66b9650b77040edd7de025cc9af40ba3
Jiashu Zou <zjsdut@163.com>
2021-10-27 15:08:01 +0800

    Add docs to xor_basis

--- library/xor_basis.hpp
+++ library/xor_basis.hpp
@@ -7,9 +7,15 @@
 #include <array>
 #include <vector>
 #include <cassert>
+
+/// @brief Maintains a linearly independent list of vectors (hereinafter the
+/// basis) in a vector space over the field F2. A vector is represented as an
+/// integer of type \p T. The dimension of the vector space is \p Dimension.
 template<typename T, std::size_t Dimension> class xor_basis {
+  // std_basis is another linearly independent list of vectors in echelon form.
   std::array<T, Dimension> std_basis_ = {};
   std::array<T, Dimension> real_basis = {};
+  // write std_basis as a linear combination of real_basis.
   std::array<T, Dimension> std_in_real = {};
   std::array<int, Dimension> order;
   int dim = 0;
@@ -17,6 +23,9 @@ template<typename T, std::size_t Dimension> class xor_basis {
 
  public:
   xor_basis() = default;
+
+  /// @brief Add a vector \p x to the basis, if \p x is a linear combination of
+  /// the current basis, remove the most recent added vector from the basis.
   void add(T x) {
     if (x == 0)
       return;
@@ -54,6 +63,7 @@ template<typename T, std::size_t Dimension> class xor_basis {
     }
   }
 
+  /// @brief Returns a subset of the basis whose sum is the vector \p x.
   std::vector<T> components(T x) const {
     assert(x > 0);
     T mask = 0;
@@ -75,6 +85,9 @@ template<typename T, std::size_t Dimension> class xor_basis {
     return result;
   }
 
+  /// @brief Returns when each of the componets of vector \p x were added to
+  /// basis. This method is not useful in general, it is for the problem abc223h
+  /// XOR Query.
   std::vector<int> component_orders(T x) const {
     assert(x > 0);
     T mask = 0;
@@ -89,7 +102,6 @@ template<typename T, std::size_t Dimension> class xor_basis {
     }
     std::vector<int> result;
     for (int i = 0; i < dim; i++) {
-      assert(i < Dimension);
       if (mask >> i & 1) {
         result.push_back(order[i]);
       }
@@ -97,6 +109,7 @@ template<typename T, std::size_t Dimension> class xor_basis {
     return result;
   }
 
+  // Returns whether a vector \p x is in the span of the basis.
   bool is_in_span(T x) const {
     for (std::size_t i = 0; i < Dimension; i++) {
       if (x >> i & 1) {
