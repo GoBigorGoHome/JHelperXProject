47a2fce5036972cab26e9426777c2ef73cbc3af1
Jiashu Zou <zjsdut@163.com>
2021-11-25 18:41:08 +0800

    [WIP] Implement ndvector
    ndvector<T, 1> is derived from std::vector<T>
    ndvector<T, 2> is derived from std::vector<std::vector<T>>
    ndvector<T, 3> is derived from std::vector<std::vector<std::vector<T>>>
    ...

---
+++ library/ndvector.hpp
@@ -0,0 +1,46 @@
+//
+// Created by zjsdu on 2021/11/25.
+//
+
+#ifndef JHELPER_EXAMPLE_PROJECT_LIBRARY_NDVECTOR_HPP_
+#define JHELPER_EXAMPLE_PROJECT_LIBRARY_NDVECTOR_HPP_
+#include <vector>
+#include <tuple_utils.hpp>
+
+// from cuiaoxiang
+template<class T> auto vect(const T& v, int n) {
+  return std::vector<T>(n, v);
+}
+
+template<class T, class... D> auto vect(const T& v, int n, D... m) {
+  return std::vector<decltype(vect(v, m...))>(n, vect(v, m...));
+}
+
+template<typename T, int... Ints>
+auto get_vect(const T& v, std::integer_sequence<int, Ints...>)
+    -> decltype(vect(v, Ints...));
+
+template<typename T, int Dimension>
+using vector_t =
+    decltype(get_vect(T(), std::make_integer_sequence<int, Dimension>()));
+
+template<typename T, int Dimension>
+class ndvector : public vector_t<T, Dimension> {
+  using base_type = vector_t<T, Dimension>;
+
+  template<typename Tuple1, typename Tuple2, std::size_t... I>
+  ndvector(const Tuple1& t1, const Tuple2& t2, std::index_sequence<I...>)
+      : base_type(vect(construct_from_tuple<T>(t1), std::get<I>(t2)...)) {}
+
+  template<typename Tuple>
+  explicit ndvector(const Tuple& t)
+      : ndvector(exclude_first_n_elements<Dimension>(t),
+                 first_n_elements<Dimension>(t),
+                 std::make_index_sequence<Dimension>{}) {}
+
+ public:
+  template<typename... Args>
+  explicit ndvector(Args... args) : ndvector(std::make_tuple(args...)) {}
+};
+
+#endif// JHELPER_EXAMPLE_PROJECT_LIBRARY_NDVECTOR_HPP_
---
+++ library/tuple_utils.hpp
@@ -0,0 +1,45 @@
+//
+// Created by zjsdu on 2021/11/25.
+//
+
+#ifndef JHELPER_EXAMPLE_PROJECT_LIBRARY_TUPLE_UTILS_HPP_
+#define JHELPER_EXAMPLE_PROJECT_LIBRARY_TUPLE_UTILS_HPP_
+
+namespace detail {
+
+template<typename Tuple, std::size_t... I>
+auto first_n_elements_impl(const Tuple& t, std::index_sequence<I...>) {
+  return std::make_tuple(std::get<I>(t)...);
+}
+
+template<std::size_t n, typename Tuple, std::size_t... I>
+auto exclude_first_n_elements_impl(const Tuple& t, std::index_sequence<I...>) {
+  return std::make_tuple(std::get<n + I>(t)...);
+}
+
+template<typename T, typename Tuple, std::size_t... I>
+T construct_from_tuple_impl(const Tuple& t, std::index_sequence<I...>) {
+  return T(std::get<I>(t)...);
+}
+
+}// namespace detail
+
+template<std::size_t n, typename Tuple,
+         typename = std::enable_if_t<std::tuple_size_v<Tuple> >= n>>
+auto first_n_elements(const Tuple& t) {
+  return detail::first_n_elements_impl(t, std::make_index_sequence<n>{});
+}
+
+template<std::size_t n, typename Tuple,
+         typename = std::enable_if_t<std::tuple_size_v<Tuple> >= n>>
+auto exclude_first_n_elements(const Tuple& t) {
+  return detail::exclude_first_n_elements_impl<n>(
+      t, std::make_index_sequence<std::tuple_size_v<Tuple> - n>{});
+}
+
+template<typename T, typename Tuple> T construct_from_tuple(const Tuple& t) {
+  return detail::construct_from_tuple_impl<T>(
+      t, std::make_index_sequence<std::tuple_size_v<Tuple>>{});
+}
+
+#endif// JHELPER_EXAMPLE_PROJECT_LIBRARY_TUPLE_UTILS_HPP_
