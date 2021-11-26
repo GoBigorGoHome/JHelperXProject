138761d568642c5db0f4693968bd2b724e8f0725
Jiashu Zou <zjsdut@163.com>
2021-11-26 17:48:06 +0800

    Revert 99c5752

--- library/io.hpp
+++ library/io.hpp
@@ -5,6 +5,15 @@
 #include <tuple>
 #include <type_traits.hpp>
 
+struct fast_ios {
+  fast_ios() {
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cout.precision(10);
+    std::cout << std::fixed;
+  };
+} const fast_ios_;
+
 namespace io {
 template<typename T, typename U>
 std::ostream &operator<<(std::ostream &out, const std::pair<T, U> &p);
