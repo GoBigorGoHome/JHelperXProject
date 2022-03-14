3d49fdb95956eb039c0e106f690c3ac4896243e4
Jiashu Zou <zjsdut@163.com>
2022-03-14 22:54:22 +0800

    Derive ndarray from std::vector

--- library/ndarray.hpp
+++ library/ndarray.hpp
@@ -4,30 +4,33 @@
 #ifndef JHELPER_EXAMPLE_PROJECT_LIBRARY_NDARRAY_HPP_
 #define JHELPER_EXAMPLE_PROJECT_LIBRARY_NDARRAY_HPP_
 #include <vector>
+template<typename T, unsigned Dimension> struct ndvec {
+  using type = std::vector<typename ndvec<T, Dimension - 1>::type>;
+};
+
+template<typename T> struct ndvec<T, 0> { using type = T; };
 
 // arbitrary-dimensional array that allows non-constexpr extents.
 // Usage: ndarray<dimension, value_type> arr(extents..., init_value);
-// All extensions but one for the last dimension are required. An initial value
-// for all array items can be specified if all extensions are present.
+// An initial value for all array items can be specified if all extensions are
+// specified.
 // Examples:
 // ndarray<2, int> a(2, 3, -1);
 // ndarray<3, int> b(2, 3, 4);
 // ndarray<3, int> c(2, 3);
 template<unsigned dimension, typename T>
-class ndarray : public std::vector<ndarray<dimension - 1, T>> {
+class ndarray : public ndvec<T, dimension>::type {
+  using base_type = typename ndvec<T, dimension>::type;
+  using value_type = typename base_type::value_type;
+  using base_type::base_type;
+
  public:
-  using std::vector<ndarray<dimension - 1, T>>::vector;
   template<typename... Args>
   ndarray(unsigned d, Args... args)
-      : std::vector<ndarray<dimension - 1, T>>(
-          d, ndarray<dimension - 1, T>(args...)) {}
+      : std::vector<value_type>(d, ndarray<dimension - 1, T>(args...)) {}
 };
 
 template<typename T> class ndarray<1, T> : public std::vector<T> {
- public:
   using std::vector<T>::vector;
-  template<typename... Args>
-  explicit ndarray(Args&&... args)
-      : std::vector<T>(std::forward<Args>(args)...) {}
 };
 #endif// JHELPER_EXAMPLE_PROJECT_LIBRARY_NDARRAY_HPP_
