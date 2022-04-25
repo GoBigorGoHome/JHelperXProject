292150c00bf0e2dd11f0a8a5fb270aff9ffcd409
Jiashu Zou <zjsdut@163.com>
2022-04-25 14:13:04 +0800

    Fix min-cost flow

--- library/min_cost_flow.hpp
+++ library/min_cost_flow.hpp
@@ -21,7 +21,7 @@ template<typename Cap, typename Cost> class MinCostFlow {
   const Cost INF_COST = std::numeric_limits<Cost>::max();
   std::vector<Cost> d;
 
-  bool dijkstra(int s, int t) {
+  Cost dijkstra(int s, int t) {
     std::fill(d.begin(), d.end(), INF_COST);
     using P = std::pair<Cost, int>;
     std::priority_queue<P, std::vector<P>, std::greater<>> q;
@@ -29,7 +29,7 @@ template<typename Cap, typename Cost> class MinCostFlow {
     while (!q.empty()) {
       auto top = q.top();
       q.pop();
-      // 由于有负权边，当一个点的d值首次作为最小值时，这个d值未必是最短路。
+      // 由于有负权边，当d[u]首次作为最小值时，d[u]未必是s到u的最短路长度。
       if (top.second == t)
         continue;
       if (d[top.second] != top.first)
@@ -42,19 +42,23 @@ template<typename Cap, typename Cost> class MinCostFlow {
         }
       }
     }
-    return d[t] != INF_COST;
+    return d[t];
   }
 
+  std::vector<bool> vis;
   std::vector<int> cur;
 
   Cap dfs(int u, Cap flow, int t) {
     if (u == t)
       return flow;
+    // 图中可能有边权之和为零的环，所以必须要有vis标记，不然dfs寻路时可能绕着边权之和为零的环一直转。
+    vis[u] = true;
     Cap pushed = 0;
     for (int& i = cur[u]; i != -1; i = e[i].next) {
       int v = e[i].to;
-      if (e[i].cap and d[v] == d[u] + e[i].cost) {
+      if (not vis[v] and e[i].cap and d[v] == d[u] + e[i].cost) {
         if (Cap f = dfs(v, std::min(flow, e[i].cap), t)) {
+          debug(u, v);
           e[i].cap -= f;
           e[i ^ 1].cap += f;
           pushed += f;
@@ -64,11 +68,12 @@ template<typename Cap, typename Cost> class MinCostFlow {
         }
       }
     }
+    vis[u] = false;
     return pushed;
   }
 
  public:
-  explicit MinCostFlow(int n) : n(n), head(n, -1), d(n) {}
+  explicit MinCostFlow(int n) : n(n), head(n, -1), d(n), vis(n) {}
 
   void add_arc(int u, int v, Cap cap, Cost cost) {
     e.push_back({v, head[u], cap, cost});
@@ -77,14 +82,26 @@ template<typename Cap, typename Cost> class MinCostFlow {
     head[v] = (int) e.size() - 1;
   }
 
-  Cap min_cost_flow(int s, int t) {
-    Cap ans = 0;
-    while (dijkstra(s, t)) {
+  std::pair<Cap, Cost> min_cost_flow(int s, int t) {
+    Cost cost = 0;
+    Cap flow = 0;
+    while (dijkstra(s, t) < 0) {
       cur = head;
-      while (Cap flow = dfs(s, std::numeric_limits<Cap>::max(), t))
-        ans += flow * d[t];
+      Cap f = dfs(s, std::numeric_limits<Cap>::max(), t);
+      cost += f * d[t];
     }
-    return ans;
+    return {flow, cost};
+  }
+
+  std::pair<Cap, Cost> min_cost_max_flow(int s, int t) {
+    Cost cost = 0;
+    Cap flow = 0;
+    while (dijkstra(s, t) != INF_COST) {
+      cur = head;
+      Cap f = dfs(s, std::numeric_limits<Cap>::max(), t);
+      cost += f * d[t];
+    }
+    return {flow, cost};
   }
 
   std::vector<Cost> slope(int s, int t) {
