eb5cfbaf2d3f3500de59991076446172d60cc866
Jiashu Zou <zjsdut@163.com>
2022-09-12 18:10:02 +0800

    Update combinatorics calc utilities

--- library/m3.hpp
+++ library/m3.hpp
@@ -6,6 +6,7 @@
 #define JHELPER_EXAMPLE_PROJECT_LIBRARY_M3_HPP_
 
 #include <vector>
+#include <cassert>
 #include <modular.hpp>
 
 using mint = Mint<998244353>;
@@ -15,16 +16,27 @@ mint operator""_m(unsigned long long v) {
 
 std::vector<mint> factorial(1, 1);
 std::vector<mint> inv_factorial(1, 1);
-mint C(int n, int m) {
-  if (m < 0 or m > n)
-    return 0;
+
+mint fact(int n) {
+  assert(n >= 0);
   while (n + 1 > (int) factorial.size()) {
     factorial.push_back(factorial.back() * (int) factorial.size());
   }
+  return factorial[n];
+}
+
+mint inv_fact(int n) {
+  assert(n >= 0);
   while (n + 1 > (int) inv_factorial.size()) {
     inv_factorial.push_back(inv_factorial.back() / (int) inv_factorial.size());
   }
-  return factorial[n] * inv_factorial[m] * inv_factorial[n - m];
+  return inv_factorial[n];
+}
+
+mint C(int n, int m) {
+  if (m < 0 or m > n)
+    return 0;
+  return fact(n) * inv_fact(m) * inv_fact(n - m);
 };
 
 template<typename Int> mint power(mint x, Int n) {
