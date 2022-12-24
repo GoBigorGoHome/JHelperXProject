2c34f8e2ac33470b4a4f6042fad0c6d3d7c829c3
Jiashu Zou <zjsdut@163.com>
2022-12-24 23:08:09 +0800

    Simplify XOR basis

--- library/xor_basis.hpp
+++ library/xor_basis.hpp
@@ -1,5 +1,5 @@
 //
-// Created by zjs on 2021/10/19.
+// Created by zjs on 12/24/22.
 //
 
 #ifndef JHELPER_EXAMPLE_PROJECT_LIBRARY_XOR_BASIS_HPP_
@@ -11,33 +11,31 @@
 /// @brief Maintains a linearly independent list of vectors (hereinafter the
 /// basis) in a vector space over the field F2. A vector is represented as an
 /// integer of type \p T. The dimension of the vector space is \p Dimension.
-template<typename T, std::size_t Dimension> class xor_basis {
+template<typename T, int Dimension> class xor_basis {
+  static_assert(Dimension > 0);
   // std_basis is another linearly independent list of vectors in echelon form.
   std::array<T, Dimension> std_basis_ = {};
   std::array<T, Dimension> real_basis = {};
   // write std_basis as a linear combination of real_basis.
   std::array<T, Dimension> std_in_real = {};
-  std::array<int, Dimension> order;
   int dim = 0;
-  int cnt = 0;
+  bool normalized = false;
 
  public:
   xor_basis() = default;
 
-  /// @brief Add a vector \p x to the basis, if \p x is a linear combination of
-  /// the current basis, remove the most recent added vector from the basis.
+  /// @brief Add a vector \p x to the basis.
   void add(T x) {
     if (x == 0)
       return;
     T y = x;
     T vec = 0;
-    for (std::size_t i = 0; i < Dimension; i++) {
+    for (int i = Dimension - 1; i >= 0; i--) {
       if (y >> i & 1) {
         if (std_basis_[i] == 0) {
           std_basis_[i] = y;
           std_in_real[i] = vec ^ ((T) 1 << dim);
           real_basis[dim] = x;
-          order[dim] = cnt++;
           dim += 1;
           return;
         }
@@ -45,29 +43,25 @@ template<typename T, std::size_t Dimension> class xor_basis {
         vec ^= std_in_real[i];
       }
     }
-    int min_order = cnt;
-    int id = -1;
-    for (int i = 0; i < dim; i++) {
-      if ((vec >> i & 1) and order[i] < min_order) {
-        min_order = order[i];
-        id = i;
-      }
-    }
-    order[id] = cnt++;
-    real_basis[id] = x;
-    const T mask = vec ^ ((T) 1 << id);
-    for (std::size_t i = 0; i < Dimension; i++) {
-      if (std_in_real[i] >> id & 1) {
-        std_in_real[i] ^= mask;
-      }
-    }
+  }
+  void normalize_std_basis() {
+    if (normalized)
+      return;
+    normalized = true;
+    for (int i = 0; i < Dimension; i++)
+      if (std_basis_[i])
+        for (int j = i + 1; j < Dimension; j++)
+          if (std_basis_[j] >> i & 1) {
+            std_basis_[j] ^= std_basis_[i];
+            std_in_real[j] ^= std_in_real[i];
+          }
   }
 
   /// @brief Returns a subset of the basis whose sum is the vector \p x.
   std::vector<T> components(T x) const {
     assert(x > 0);
     T mask = 0;
-    for (std::size_t i = 0; i < Dimension; i++) {
+    for (int i = 0; i < Dimension; i++) {
       if (x >> i & 1) {
         if (std_basis_[i] == 0) {
           return {};
@@ -80,30 +74,6 @@ template<typename T, std::size_t Dimension> class xor_basis {
     for (int i = 0; i < dim; i++) {
       if (mask >> i & 1) {
         result.push_back(real_basis[i]);
-      }
-    }
-    return result;
-  }
-
-  /// @brief Returns when each of the components of vector \p x were added to
-  /// basis. This method is not useful in general, it is for the problem abc223h
-  /// XOR Query.
-  std::vector<int> component_orders(T x) const {
-    assert(x > 0);
-    T mask = 0;
-    for (std::size_t i = 0; i < Dimension; i++) {
-      if (x >> i & 1) {
-        if (std_basis_[i] == 0) {
-          return {};
-        }
-        x ^= std_basis_[i];
-        mask ^= std_in_real[i];
-      }
-    }
-    std::vector<int> result;
-    for (int i = 0; i < dim; i++) {
-      if (mask >> i & 1) {
-        result.push_back(order[i]);
       }
     }
     return result;
@@ -126,7 +96,13 @@ template<typename T, std::size_t Dimension> class xor_basis {
     return {real_basis.begin(), real_basis.begin() + dim};
   }
 
-  auto std_basis() const { return std_basis_; }
+  auto std_basis() const {
+    std::vector<T> ans;
+    for (int i = 0; i < Dimension; i++)
+      if (std_basis_[i])
+        ans.push_back(std_basis_[i]);
+    return ans;
+  }
 
   int dimension() const { return dim; }
 };
