018c8dff9bdd406c4bc5fe1bad672de9e4023ee2
Jiashu Zou <zjsdut@163.com>
2023-01-18 09:11:56 +0800

    Rename member variables and member types

--- library/modular_hash.hpp
+++ library/modular_hash.hpp
@@ -15,9 +15,10 @@
 /// positive integer.
 template<int modulus> class modular_hash {
   int length = 0;
-  using value_type = Mint<modulus>;
-  std::vector<value_type> value;
-  std::vector<value_type> power;
+  int radix_;
+  using mint = Mint<modulus>;
+  std::vector<mint> value;
+  std::vector<mint> pow;
 
  public:
   modular_hash() = default;
@@ -26,44 +27,51 @@ template<int modulus> class modular_hash {
   /// @param radix the radix chosen for hash function
   template<typename String>
   modular_hash(const String &a, int radix, int offset = 0)
-      : length((int) std::size(a)) {
+      : length((int) std::size(a)), radix_(radix) {
     assert(length > 0);
     assert(radix > 0);
     value.resize(length);
-    power.resize(length);
-    value_type sum = 0;
+    pow.resize(length);
+    mint sum = 0;
     for (int i = 0; i < length; i++) {
       assert(a[i] >= offset);
       assert(a[i] - offset < radix);
       sum = sum * radix + (a[i] - offset);
       value[i] = sum;
     }
-    power[0] = 1;
+    pow[0] = 1;
     for (int i = 1; i < length; i++) {
-      power[i] = radix * power[i - 1];
+      pow[i] = radix * pow[i - 1];
     }
   }
   /// @brief Returns hash value of a substring [\p pos, \p pos + \p count).
   /// @param p position of the first character to include
   /// @param count length of the substring
-  value_type operator()(int pos, int count) const {
+  mint operator()(int pos, int count) const {
     assert(count >= 0);
     assert(0 <= pos and pos + count <= length);
     if (count == 0)
       return 0;
     return pos == 0 ? value[count - 1]
-                    : value[pos + count - 1] - power[count] * value[pos - 1];
+                    : value[pos + count - 1] - pow[count] * value[pos - 1];
   }
   /// @brief Returns hash value of a given string.
   /// @param key the string to be hashed
   template<typename String>
-  value_type operator()(const String &key, int radix, int offset = 0) const {
-    value_type res = 0;
+  mint operator()(const String &key, int radix, int offset = 0) const {
+    mint res = 0;
     for (auto x : key) {
       assert(offset <= x and x - offset < radix);
       res = res * radix + (x - offset);
     }
     return res;
+  }
+  // 返回radix的n次幂。
+  mint power(int n) const {
+    assert(n >= 0);
+    if (n < length)
+      return pow[n];
+    return Power(mint(radix_), n);
   }
 };
 #endif// JHELPER_EXAMPLE_PROJECT_LIBRARY_MODULAR_HASH_HPP_
