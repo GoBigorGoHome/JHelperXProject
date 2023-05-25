82a320c143e44b933e4080bd83743526b48d90ab
Jiashu Zou <zjsdut@163.com>
2023-05-25 15:48:41 +0800

    Update dsu_with_rollback
    * count every merge
    * maintain number of connected components that are trees,
       for abc302h

--- library/dsu_with_rollback.hpp
+++ library/dsu_with_rollback.hpp
@@ -7,18 +7,25 @@
 
 #include <vector>
 #include <cassert>
+#include <tuple>
+#include <algorithm>
 
 class dsu_with_rollback {
-  std::vector<int> ps;                    // parent or size
-  std::vector<std::pair<int, int>> record;// merge record
-  int ecnt = 0;                           // number of edges added
+  std::vector<int> ps;// parent or size
+  std::vector<bool> is_tree;
+  std::vector<std::tuple<int, int, bool>> record;// merge record
+  int treeCnt;// number of connected components that are trees
+  int n;
 
  public:
-  explicit dsu_with_rollback(int n) : ps(n, -1), record(n - 1) {}
+  explicit dsu_with_rollback(int n)
+      : ps(n, -1), is_tree(n, true), treeCnt(n), n(n) {}
 
   void clear() {
-    fill(ps.begin(), ps.end(), -1);
-    ecnt = 0;
+    std::fill(ps.begin(), ps.end(), -1);
+    std::fill(is_tree.begin(), is_tree.end(), true);
+    record.clear();
+    treeCnt = n;
   }
 
   int root(int u) const {
@@ -29,31 +36,53 @@ class dsu_with_rollback {
 
   int size(int u) const { return -ps[root(u)]; }
 
-  int edge_cnt() const { return ecnt; }
+  int tree_cnt() const { return treeCnt; }
+
+  int edge_cnt() const { return (int) record.size(); }
 
   bool merge(int u, int v) {
     u = root(u);
     v = root(v);
-    if (u == v)
-      return 0;
+    if (u == v) {
+      record.push_back({u, 0, is_tree[u]});
+      treeCnt -= is_tree[u];
+      is_tree[u] = false;
+      return false;
+    }
     if (ps[u] < ps[v])
       std::swap(u, v);
+    record.push_back({u, ps[u], is_tree[v]});
     ps[v] += ps[u];
-    record[ecnt++] = {u, ps[u]};
     ps[u] = v;
-    return 1;
+    if (is_tree[u] || is_tree[v])
+      treeCnt--;
+    if (is_tree[u] != is_tree[v])
+      is_tree[v] = false;
+    return true;
   }
 
   void rollback() {
-    assert(ecnt > 0);
-    auto [u, sz] = record[--ecnt];
-    int v = ps[u];
-    ps[u] = sz;
-    ps[v] -= sz;
+    assert(!record.empty());
+    auto [u, sz, wasTree] = record.back();
+    record.pop_back();
+
+    if (sz == 0) {
+      is_tree[u] = wasTree;
+      if (is_tree[u])
+        treeCnt++;
+    } else {
+      int v = ps[u];
+      ps[u] = sz;
+      ps[v] -= sz;
+      is_tree[v] = wasTree;
+      if (is_tree[u] || is_tree[v])
+        treeCnt++;
+    }
   }
 
   void rollback_to(int edge_cnt) {
-    while (ecnt > edge_cnt)
+    assert(edge_cnt >= 0);
+    while ((int) record.size() > edge_cnt)
       rollback();
   }
 };
