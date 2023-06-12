40069501101977105a5cd70a2b4d385b16647780
Jiashu Zou <zjsdut@163.com>
2023-06-12 14:49:07 +0800

    Refactor DSU
    change method name

--- library/dsu.hpp
+++ library/dsu.hpp
@@ -10,31 +10,34 @@ class UnionFind {
   explicit UnionFind(int n)
       : parent_or_size(n, -1), nTree(n), has_cycle_(n) {// 0-indexed
   }
-  void init() {
+
+  void reset() {
     std::fill(parent_or_size.begin(), parent_or_size.end(), -1);
     std::fill(has_cycle_.begin(), has_cycle_.end(), false);
     nTree = (int) parent_or_size.size();
   }
+
   int n_tree() const { return nTree; }
 
-  int size(int x) { return -parent_or_size[root(x)]; }
+  int size(int x) { return -parent_or_size[leader(x)]; }
 
   /// @brief Reset the size of the tree where x belongs to 1. This method is not
   /// for general use.
-  void reset_size(int x) { parent_or_size[root(x)] = -1; }
+  void reset_size(int x) { parent_or_size[leader(x)] = -1; }
 
-  bool has_cycle(int x) { return has_cycle_[root(x)]; }
+  bool has_cycle(int x) { return has_cycle_[leader(x)]; }
 
-  int root(int x) {
-    return parent_or_size[x] < 0 ? x
-                                 : parent_or_size[x] = root(parent_or_size[x]);
+  int leader(int x) {
+    return parent_or_size[x] < 0
+        ? x
+        : parent_or_size[x] = leader(parent_or_size[x]);
   }
   //! @brief Merge tree y into tree x.
   //! @param f Function for extra processing, called as f(x, y).
   template<typename F = void (*)(int, int)>
-  bool unite(
+  bool merge(
       int x, int y, const F &f = [](int, int) {}) {
-    int rx = root(x), ry = root(y);
+    int rx = leader(x), ry = leader(y);
     if (rx != ry) {
       parent_or_size[rx] += parent_or_size[ry];
       parent_or_size[ry] = rx;
@@ -47,6 +50,7 @@ class UnionFind {
     return false;
   }
 
-  bool is_root(int x) const { return parent_or_size[x] < 0; }
-  bool same(int x, int y) { return root(x) == root(y); }
+  bool is_leader(int x) const { return parent_or_size[x] < 0; }
+
+  bool same(int x, int y) { return leader(x) == leader(y); }
 };
