a092e99a5ee937be7428acaa8ea4671f3c0c6104
Jiashu Zou <zjsdut@163.com>
2024-03-13 15:56:19 +0800

    Add lca and level ancestor to HLD

--- library/tree/hld.hpp
+++ library/tree/hld.hpp
@@ -8,6 +8,7 @@
 // 例题：https://cses.fi/problemset/task/2134
 // how to use: hld_init(n); add_edges(u, v); hld_build();
 #include <vector>
+#include <cassert>
 
 bool ready_ = false;
 bool init_ = false;
@@ -19,6 +20,7 @@ std::vector<int> dep;
 std::vector<int> top;
 std::vector<int> sz;
 std::vector<int> id;
+std::vector<int> node_;
 
 void hld_init(int n) {
   n_ = n;
@@ -28,6 +30,7 @@ void hld_init(int n) {
   top.assign(n, 0);
   sz.assign(n, 0);
   id.assign(n, 0);
+  node_.assign(n, 0);
   init_ = true;
 }
 
@@ -44,6 +47,7 @@ void sz_dfs(int u) {
 }
 
 void hld_dfs(int u, int _top, int p = -1) {
+  node_[id_] = u;
   id[u] = id_++;
   top[u] = _top;
   int hc = -1;// heavy child
@@ -73,4 +77,25 @@ void hld_build() {
   hld_dfs(0, 0);
   ready_ = true;
 }
+
+int lca(int a, int b) {
+  while (top[a] != top[b]) {
+    if (dep[top[a]] > dep[top[b]])
+      a = pv[top[a]];
+    else
+      b = pv[top[b]];
+  }
+  return dep[a] < dep[b] ? a : b;
+}
+
+int ancestor(int a, int k) {
+  if (k > dep[a])
+    return -1;
+  while (k > dep[a] - dep[top[a]]) {
+    k -= dep[a] - dep[top[a]] + 1;
+    a = pv[top[a]];
+  }
+  return node_[id[a] - k];
+}
+
 #endif// JHELPER_EXAMPLE_PROJECT_LIBRARY_TREE_HLD_HPP_
