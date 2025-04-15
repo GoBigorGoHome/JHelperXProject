347a22a78f7fae462ed1d7f5b7c31648fd02341a
Jiashu Zou <zjsdut@163.com>
2025-04-15 20:41:28 +0800

    Update 坐标压缩

--- library/coordinate_compress.hpp
+++ library/coordinate_compress.hpp
@@ -9,28 +9,41 @@
 #include <algorithm>
 #include <numeric>
 
-// 坐标压缩
+/// 坐标压缩。
+/// return a compressed version of a, i.e. maps every element of a to an
+/// integer, starting from 0, preserving order.
 template<typename T>
-std::vector<int> compress(const std::vector<T>& a) {
+std::vector<int> compress(const std::vector<T> &a) {
   int n = (int) a.size();
   std::vector<int> I(n);
   std::iota(I.begin(), I.end(), 0);
-  std::sort(I.begin(), I.end(),
-            [&a](int i, int j) { return a[i] < a[j]; });
+  std::sort(I.begin(), I.end(), [&a](int i, int j) { return a[i] < a[j]; });
   std::vector<int> order(n);
   for (int i = 1; i < n; i++)
     order[I[i]] = order[I[i - 1]] + (a[I[i - 1]] < a[I[i]]);
   return order;
 }
 
-// 坐标压缩并且unique
+/// return a compressed version of a, relative to b, i.e. maps each a[i] to
+/// number of **distinct** elements of b that is less than a[i].
 template<typename T>
-std::vector<int> compress_unique(std::vector<T>& a) {
+std::vector<int> compress(const std::vector<T> &a, std::vector<T> b) {
+  std::sort(b.begin(), b.end());
+  auto end = std::unique(b.begin(), b.end());
+  std::vector<int> res(a.size());
+  for (int i = 0; i < (int) a.size(); i++)
+    res[i] = (int) (std::lower_bound(b.begin(), end, a[i]) - b.begin());
+  return res;
+}
+
+/// return a compressed version of a, make a sorted and remove duplicated
+/// elements of a.
+template<typename T>
+std::vector<int> compress_unique(std::vector<T> &a) {
   int n = (int) a.size();
   std::vector<int> I(n);
   std::iota(I.begin(), I.end(), 0);
-  std::sort(I.begin(), I.end(),
-            [&a](int i, int j) { return a[i] < a[j]; });
+  std::sort(I.begin(), I.end(), [&a](int i, int j) { return a[i] < a[j]; });
   std::vector<int> order(n);
   std::vector<T> b(n);
   b[0] = a[I[0]];
@@ -45,4 +58,4 @@ std::vector<int> compress_unique(std::vector<T>& a) {
   return order;
 }
 
-#endif //JHELPER_EXAMPLE_PROJECT_LIBRARY_COORDINATE_COMPRESS_HPP_
+#endif// JHELPER_EXAMPLE_PROJECT_LIBRARY_COORDINATE_COMPRESS_HPP_
